f7a756ac59814007a0237a8bc2cc78eca4341d6b
RaihanArefin <raihanarefincoc1@gmail.com>
2019-11-30 18:31:34 +0600

    confirmed working

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Laaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          Gulu
         </a>
       </header>
     </div>
