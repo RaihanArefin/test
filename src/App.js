3cffb28e22a245d1f08d26cfb534159d94cdbdb2
RaihanArefin <raihanarefincoc1@gmail.com>
2019-12-02 17:15:56 +0600

    confirmed working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+const marked = require('marked');
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // a header (H1 size), a sub header (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text
+      markdown: "# h1\n## h2\n[la](https://www.google.com/)\n`<div></div>`\n```functionw\ndgsdgsd\nvsdv\n```\n- list\n>block codes\n![](https://commonmark.org/help/images/favicon.png)\n **bold**"
+
+    }
+
+
+    this.update = this.update.bind(this);
+  }
+
+
+  update = function (event) {
+    this.setState({
+      markdown: event.target.value
+    });
+  }
+  render() {
+    const { markdown } = this.state;
+    return (
+      <div className="row App container-fluid">
+        {/* ------------Input---------------  */}
+        <div className="col-md-6">
+          <h1>Input</h1>
+          <textarea id="editor" className="la" value={markdown} onChange={(event) => this.update(event)}></textarea>
+        </div>
+
+        {/* ----------------OUTPUT---------------- */}
+        <div className="col-md-6">
+          <h1>Output</h1>
+          <div id="preview" className="la" dangerouslySetInnerHTML={{ __html: marked(markdown) }}>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
